7ecbcff97cd90a96c70a670731456e0d75b63034
Ethan <38564426+skyventor@users.noreply.github.com>
2020-01-15 20:17:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,154 +1,177 @@
 <!DOCTYPE html>
 <html>	
-  <head>
-    <link rel = "stylesheet" type="text/css" href= "/Map/style.css" media="all"/>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>Pickup</title>
-    
-    
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
-  </head>
+<head>
+  <link rel = "stylesheet" type="text/css" href = "theme.css" media="all"/>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Pickup</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
 
-  <body>
+<body>
 
-    <h1 class="TitleText">Map Plotter</h1>
+  <div id = "container">
 
+    <!--TOP BAR-->
+    <div class = "title">
+      <h1 class="TitleText">PICKUP FINDER</h1>
+    </div> 
 
-    <div id = "Submission">
+    <div class = "spacer"></div>
+
+    <div id = "content">
       <form id = "Form" action="insert.php" method="POST">
-        Event Title:<br>
-        <input type="text" name="title"/><br><br>
+        <label class="label">
+          Event Title:<br>
+          <input type="text" name="title"/><br><br>
+        </label>
 
-        Event Description:<br>
-        <input type="text" name="about" id="desc"/><br><br>
+        <label class="label">
+          Event Description:<br>
+          <input type="text" name="about" id="desc"/><br><br>
+        </label>  
 
-        Event Date:<br>
-        <input type="datetime-local" name="date" /><br><br>
+        <label class="label">
+          Event Date:<br>
+          <input type="datetime-local" name="date" /><br><br>
+        </label>
 
-        Event Location:<br>
-        <input type="text" name="location"/><br><br>
+        <label class="label">
+          Event Location:<br>
+          <input type="text" name="location"/><br><br>
+        </label>
 
-        Latitude:<br>
-        <input type="text" name="lat"/><br><br>
+        <label class="label">
+          Latitude:<br>
+          <input type="text" name="lat"/><br><br>
+        </label>
 
-        Longitude:<br>
-        <input type="text" name="long"/><br><br>
+        <label class="label">
+          Longitude:<br>
+          <input type="text" name="long"/><br><br>
+        </label>
 
-        <input type="submit" name = "register_btn" id = "submitBtn"/><br>
-        
+        <input type="submit" name = "register_btn" class = "submitBtn"/><br>
       </form>
-
     </div>
+    <div class = "spacer"></div>
+  </div>
 
-    <?php
-    
-      $lat = array();
-      $lon = array();
-      $coordinates = array();
-		  $link = mysqli_connect("localhost", "root", "", "data");
+  <?php
+    $lat = array();
+    $lon = array();
+    $coordinates = array();
+    $link = mysqli_connect("localhost", "root", "root", "data",3306);
 
-      if (!$link) {
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
+    if (!$link) {
+      echo "Error: Unable to connect to MySQL." . PHP_EOL;
+      echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+      echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+      exit;
+    }
+
+    $sql = "SELECT latitude,longitude,title,about,date,address FROM events ORDER BY ID ASC";
+    $result = $link->query($sql);
+
+    if($result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+        $lat = $row["latitude"];
+        $lon = $row["longitude"];
+        $name = $row["title"];
+        $about = $row["about"];
+        $date = $row["date"];
+        $loc = $row["address"];
+
+        $coordinates[]=array($lat, $lon, $name, $about, $date, $loc);
       }
-  
-      $sql = "SELECT latitude,longitude,title,about,date,address FROM events ORDER BY ID ASC";
-      $result = $link->query($sql);
+      $locations = json_encode($coordinates);
+    }
+    else {
+      echo "0 results";
+    }
 
-      if($result->num_rows > 0){
-        while($row = $result->fetch_assoc()){
-          $lat = $row["latitude"];
-          $lon = $row["longitude"];
-          $name = $row["title"];
-          $about = $row["about"];
-          $date = $row["date"];
-          $loc = $row["address"];
+    $link->close();
+  ?>
 
-          $coordinates[]=array($lat, $lon, $name, $about, $date, $loc);
-        }
-        $locations = json_encode($coordinates);
-      }
-      else {
-        echo "0 results";
-      }
+  <div id="map">
+    <script type="text/javascript">
+      //some variables
+      var lat = <?php echo json_encode($lat);?>;
+      var lon = <?php echo json_encode($lon);?>;
 
-      $link->close();
-    
-    ?>
+      //locations as a list
+      <?php
+        echo "var locations=$locations;\n";
+      ?>
+      
+      //function for map
+      
+      function initMap() {
+        var map;
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 38.487995, lng: -99.191275},
+          zoom: 4
+        });
 
-    <div id="map">
-      <script type="text/javascript">
-        //some variables
-        var lat = <?php echo json_encode($lat);?>;
-        var lon = <?php echo json_encode($lon);?>;
+        var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+        var icons = {
+          basketball: {
+            icon: iconBase + 'basketball2.png'
+          }
+        };
 
-        //locations as a list
-        <?php
-          echo "var locations=$locations;\n";
-        ?>
+        var marker,i;
+        var markers = new Array();
         
-        //function for map
         
-        function initMap() {
-          var map;
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 38.487995, lng: -99.191275},
-            zoom: 4
+        for (i = 0; i < locations.length; i++) { 
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+            //icon: icons['basketball'].icon,
+            map:map
           });
 
-          var marker,i;
-          var markers = new Array();
+          markers.push(marker);
 
-          
-          for (i = 0; i < locations.length; i++) { 
-            marker = new google.maps.Marker({
-              position: new google.maps.LatLng(locations[i][0], locations[i][1]),
-              map: map
-            });
+          //initializing the infoWindow
+          var infowindow = new google.maps.InfoWindow({
+            maxWidth: 500,
+            maxHeight: 300
+          });
 
-            markers.push(marker);
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              var windowContent = 
+              'Name: ' + locations[i][2] + '<br>' + 
+              'About: ' + locations[i][3] + '<br>' + 
+              'Date and Time: ' + locations[i][4] + '<br>' +
+              'Location: ' + locations[i][5];
 
-            //initializing the infoWindow
-            var infowindow = new google.maps.InfoWindow({
-              maxWidth: 500,
-              maxHeight: 300
-            });
-
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-              return function() {
-                var windowContent = 
-                'Name: ' + locations[i][2] + '<br>' + 
-                'About: ' + locations[i][3] + '<br>' + 
-                'Date and Time: ' + locations[i][4] + '<br>' +
-                'Location: ' + locations[i][5];
-
-                infowindow.setContent(windowContent);
-                infowindow.open(map, marker);
-              } 
-            })(marker, i));
-
-          }
-
-          function AutoCenter() {
-            var bounds = new google.maps.LatLngBounds();
-            $.each(markers, function (index, marker) {
-            bounds.extend(marker.position);
-            });
-            
-            map.fitBounds(bounds);
-          }
-          AutoCenter();
+              infowindow.setContent(windowContent);
+              infowindow.open(map, marker);
+            } 
+          })(marker, i));
         }
 
-      </script>
+        function AutoCenter() {
+          var bounds = new google.maps.LatLngBounds();
+          $.each(markers, function (index, marker) {
+          bounds.extend(marker.position);
+          });
+          
+          map.fitBounds(bounds);
+        }
+        AutoCenter();
+      }
 
-    </div>
-    
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFMtt2pb3rmfAaXj3S3-4vWO5ajd5j3So&callback=initMap"></script>
+    </script>
 
-	  <br>
-  </body>
+  </div>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFMtt2pb3rmfAaXj3S3-4vWO5ajd5j3So&callback=initMap"></script>
+
+  <div class="footer">
+    <p>© 2020 Sky Ventor</p><br>
+  </div>
+  
+</body>
 </html>
